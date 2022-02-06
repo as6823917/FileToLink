8607a52ce6c0a9d028de1d0471f0caf1116376bc
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-02-06 18:38:56 +0530

    Update del.py

--- FileToLink/del.py
+++ FileToLink/del.py
@@ -1,4 +1,17 @@
-from config import TIME
+
+import pyrogram
+import asyncio
+
+from asyncio import sleep as slp
+from pyrogram import Client, filters
+from pyrogram.types import User, Message
+
+from info import API_ID
+from info import API_HASH
+from info import SESSION
+from info import ADMINS
+from info import TIME
+from  import GROUPS
 
 
 @app.on_message(filters.group & filters.chat(GROUPS) & filters.all)
