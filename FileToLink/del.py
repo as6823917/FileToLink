d45c9ea485be0dd0498fe116ee7b3ef5ad48be53
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-02-06 18:51:27 +0530

    Update del.py

--- FileToLink/del.py
+++ FileToLink/del.py
@@ -20,7 +20,7 @@ bot = Client(
 )
 
 
-@bot.on_message(filters.command('deletemessage') & filters.group & filters.chat(GROUPS) & filters.all)
+@bot.on_message(filters.command("deletemessage") & filters.group & filters.chat(GROUPS) & filters.all)
 async def deleter(bot: Client, cmd: Message):
          if cmd.from_user.id not in ADMINS:
                   await slp(int(TIME))
