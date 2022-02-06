466e2968b22464bd754ef6df8b027c9c44fc9de1
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-02-06 18:43:48 +0530

    Update del.py

--- FileToLink/del.py
+++ FileToLink/del.py
@@ -6,15 +6,15 @@ from asyncio import sleep as slp
 from pyrogram import Client, filters
 from pyrogram.types import User, Message
 
-from info import API_ID
-from info import API_HASH
-from info import SESSION
-from info import ADMINS
-from info import TIME
-from  import GROUPS
+from config import API_ID
+from config import API_HASH
+from config import SESSION
+from config import ADMINS
+from config import TIME
+from config import GROUPS
 
 
-@app.on_message(filters.group & filters.chat(GROUPS) & filters.all)
+@bot.on_message(filters.group & filters.chat(GROUPS) & filters.all)
 async def deleter(bot: Client, cmd: Message):
          if cmd.from_user.id not in ADMINS:
                   await slp(int(TIME))
